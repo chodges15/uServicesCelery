af488b38dbde1055af75280e0f5051d12acf48fc
Chris Hodges <chris.hodges@adtran.com>
2019-10-06 21:29:12 -0500

    Web interface up on port 80

---
+++ src/http_interface/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# turn on bash's job control
+set -m
+
+# Start the Flask frontend
+python ./http_interface.py&
+
+# Start the Celery worker for the frontend
+celery --app=http_interface.celery worker --concurrency=10 --loglevel=INFO
--- src/http_interface/http_interface.py
+++ src/http_interface/http_interface.py
@@ -24,5 +24,5 @@ def add(x, y):
     return x + y
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host= '0.0.0.0')
  
